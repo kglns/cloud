8092fe8b8aaa13d91991bbabf2bc8f18df5e46ec
Kaung Lin <kaung.lin@gmail.com>
2020-04-17 11:39:16 +0630

    Script for installing ruby and postgres

---
+++ install_ruby_and_postgres.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Install ruby
+RUBY_VERSION="2.7.1"
+RAILS_VERSION="6.0.2.2"
+
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+sudo apt-get update
+sudo apt-get --yes install zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn
+
+cd
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+exec $SHELL
+
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+exec $SHELL
+
+rbenv install $RUBY_VERSION
+rbenv global $RUBY_VERSION
+
+ruby -v
+
+gem install bundler
+gem install rails -v "RAILS_VERSION"
+
+rbenv rehash
+
+rails -v
+
+# Install postgres
+apt-get --yes install postgresql-11 libpq-dev
